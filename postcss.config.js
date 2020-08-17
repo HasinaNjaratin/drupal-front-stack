9a76f2fcd8f3577c53d89ec4052be77bfc5e3929
Hasina RAKOTOVAHOAKA <hasina.mailto@gmail.com>
2020-08-17 19:33:44 +0200

    Fix vulnerabilities

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,6 @@ module.exports = {
     sourceMap: true,
     plugins: [
         require('autoprefixer'),
-        require('postcss-custom-properties'),
         require('postcss-css-variables'),
         require('postcss-mixins'),
         require('postcss-nested')
